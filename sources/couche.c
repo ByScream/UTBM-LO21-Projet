5d5b00967ca86a265cc126bc4e116b656391029f
ByScream_ <mathis.richard@sfr.fr>
2024-11-05 14:07:23 +0100

    mhhh

--- sources/couche.c
+++ sources/couche.c
@@ -11,10 +11,12 @@ Couche initCouche(int nbNeurones, int nbEntrees) {
     neurone* tmp;
     printf("---- premier neurone \n");
     neurone* premier = initNeur(nbEntrees);
+    premier->next=NULL;
     tmp = premier;
     for (int i=1; i<nbNeurones; i++) {
         printf("---- %deme neurone \n",i+1);
         neurone* newNeurone = initNeur(nbEntrees);
+        newNeurone->next=NULL;
         tmp->next=newNeurone;
     }
     return premier;
