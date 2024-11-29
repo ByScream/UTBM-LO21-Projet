30b9c5f71f17f42d23f69a7350a085b55dcb9eee
ByScream_ <mathis.richard@sfr.fr>
2024-11-29 15:09:37 +0100

    Ajout de commentaires

--- sources/main.c
+++ sources/main.c
@@ -5,6 +5,8 @@
 #include "reseau.h"
 
 int main(void) {
+
+    //Paramétrage des différentes entrées possibles
     int listeEntrees1[]={1,0,0};
     int listeEntrees2[]={0,1,0};
     int listeEntrees3[]={0,0,1};
@@ -13,8 +15,14 @@ int main(void) {
     int listeEntrees6[]={0,1,1};
     int listeEntrees7[]={0,0,0};
     int listeEntrees8[]={1,1,1};
+
+    //Liste des neurones sur chaque couche
     int listeNeurones[]={4,2,1};
+
+    //On créé le réseau, en définissant le nombre de couches, et la liste de neurones
     Reseau reseau = CreerResNeur(3,listeNeurones);
+
+    //On fait la propagation avant sur le réseau, pour chaque entrée, et on observe le résultat
     printf("---- Entrée = [1,0,0] :\n");
     PropagationAvant(reseau,listeEntrees1);
     printf("---- Entrée = [0,1,0] :\n");
